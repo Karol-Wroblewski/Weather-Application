2dba3e0afdea20b4fe1e6c2f25a1356b28ee9b85
Karol-Wroblewski <Karol-Wroblewski@users.noreply.github.com>
2019-05-22 19:30:55 +0200

    Updates

---
+++ precache-manifest.1e2a39ea2d90133b56d9b501ef253826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64bdd43a6bcf4daa0a86f2040598fa7",
+    "url": "/Weather-Application/index.html"
+  },
+  {
+    "revision": "22dd2777f53936d6540d",
+    "url": "/Weather-Application/static/css/main.2bd30e8c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Weather-Application/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "22dd2777f53936d6540d",
+    "url": "/Weather-Application/static/js/main.d7fa3d69.chunk.js"
+  },
+  {
+    "revision": "90394af5d372f6e0bc63",
+    "url": "/Weather-Application/static/js/runtime~main.d68c09e2.js"
+  },
+  {
+    "revision": "1aacd0d320538f8c2dfff4c1f2ed774c",
+    "url": "/Weather-Application/static/media/sky.1aacd0d3.jpg"
+  }
+]);
